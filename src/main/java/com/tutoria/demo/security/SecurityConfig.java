6d4b921c7524aa112d01e762767816d0c8e4fbc7
Brianmax <george.maxi@ucsp.edu.pe>
2024-02-20 00:12:32 -0500

    jwt package

---
+++ src/main/java/com/tutoria/demo/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.tutoria.demo.security;
+
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
