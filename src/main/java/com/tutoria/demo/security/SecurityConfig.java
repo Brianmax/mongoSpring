4f7ae1c94febca37443d143b72e564a295d5aa5d
Brianmax <george.maxi@ucsp.edu.pe>
2024-02-20 00:17:45 -0500

    security filter chain config

--- src/main/java/com/tutoria/demo/security/SecurityConfig.java
+++ src/main/java/com/tutoria/demo/security/SecurityConfig.java
@@ -2,10 +2,27 @@ package com.tutoria.demo.security;
 
 import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(c->c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(c->c.requestMatchers("/api/v1/product/bye")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        return http.build();
+    }
 }
