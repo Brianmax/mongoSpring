ca2e919ba2017031e29b5f1bf405671f7b558327
drafos190 <brianmaxi32@gmail.com>
2024-02-21 02:00:09 +0000

    controller fixed

--- src/main/java/com/tutoria/demo/controller/UsuarioController.java
+++ src/main/java/com/tutoria/demo/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class UsuarioController {
         return usuarioService.addProduct2User(iduser, idProduct);
     }
     @GetMapping("/findAll")
-    public Usuario findAllUsers(Pageable pageable)
+    public Page<Usuario> findAllUsers(Pageable pageable)
     {
 	    return usuarioService.findAll(pageable);
     }
